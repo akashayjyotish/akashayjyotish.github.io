96fbf048be69fb9b85b68e02ed49f6ba6f8453a7
akashayjyotish <51835894+akashayjyotish@users.noreply.github.com>
2019-06-17 12:09:14 +0530

    initial

---
+++ precache-manifest.ddc819c92f411de38625be83b052d2c1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b658e97d496dd7f89d5fbe9870f24436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10aec535ede53c427956",
+    "url": "/static/css/2.da4caab4.chunk.css"
+  },
+  {
+    "revision": "827ffeff7087f02c48f6",
+    "url": "/static/css/main.8a008981.chunk.css"
+  },
+  {
+    "revision": "10aec535ede53c427956",
+    "url": "/static/js/2.94819499.chunk.js"
+  },
+  {
+    "revision": "827ffeff7087f02c48f6",
+    "url": "/static/js/main.afb5b766.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc709d3d4eeefa1484cdda9a7bebfd4d",
+    "url": "/static/media/aquarius.bc709d3d.png"
+  },
+  {
+    "revision": "92cfc2e2b89e989b9d2f37dd8e70922d",
+    "url": "/static/media/aries.92cfc2e2.png"
+  },
+  {
+    "revision": "5cea93ee390aa5328611a82fa5c10694",
+    "url": "/static/media/cancer.5cea93ee.png"
+  },
+  {
+    "revision": "1dcebed01b19363c45f1d46cf875e234",
+    "url": "/static/media/capricorn.1dcebed0.png"
+  },
+  {
+    "revision": "8fcc6197a73da9628ae0958438669193",
+    "url": "/static/media/gemini.8fcc6197.png"
+  },
+  {
+    "revision": "2cf2d29fdaa6fc12c1fb031e0dec8db0",
+    "url": "/static/media/headPhoto.2cf2d29f.jpeg"
+  },
+  {
+    "revision": "79071caeae292d5cf0ad393e4a2b347e",
+    "url": "/static/media/img1.79071cae.jpeg"
+  },
+  {
+    "revision": "9d5e2b3d6b963044132bb16f59f145ac",
+    "url": "/static/media/img2.9d5e2b3d.jpeg"
+  },
+  {
+    "revision": "249016182be16f037add5b878ef34bb2",
+    "url": "/static/media/img3.24901618.jpeg"
+  },
+  {
+    "revision": "4e5e5ecc3ec0a8a64042f0d54fef0879",
+    "url": "/static/media/img4.4e5e5ecc.jpeg"
+  },
+  {
+    "revision": "8bf479f86a7371b78f932f29f5c8bca0",
+    "url": "/static/media/img5.8bf479f8.jpeg"
+  },
+  {
+    "revision": "3eaa4720de4c3b167c8416a3634dc618",
+    "url": "/static/media/img6.3eaa4720.jpeg"
+  },
+  {
+    "revision": "197ade46ce3202d7b09d32970788ccab",
+    "url": "/static/media/leo.197ade46.png"
+  },
+  {
+    "revision": "b222687309b03b8b1966cc1f8f9c72ca",
+    "url": "/static/media/libra.b2226873.png"
+  },
+  {
+    "revision": "ba92cbdae8d0c64217132394768d28f1",
+    "url": "/static/media/pisces.ba92cbda.png"
+  },
+  {
+    "revision": "70cbe3d8d49600c38358ad101e90cf59",
+    "url": "/static/media/sagittarius.70cbe3d8.png"
+  },
+  {
+    "revision": "925ced35d2649da09865225cdcdeca37",
+    "url": "/static/media/scorpio.925ced35.png"
+  },
+  {
+    "revision": "a751bed795a359724ea1de0ce6918dd9",
+    "url": "/static/media/taurus.a751bed7.png"
+  },
+  {
+    "revision": "5d58ee29d5fa53dc3d2ed05d63dc3f45",
+    "url": "/static/media/virgo.5d58ee29.png"
+  }
+]);
